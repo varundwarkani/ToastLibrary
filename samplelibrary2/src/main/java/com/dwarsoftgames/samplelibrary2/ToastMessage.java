14b9966b2fb9d3f3b39966b3227239256cf35d24
Varun <varundwarkani1@gmail.com>
2019-06-26 01:02:07 +0530

    Initial Commit

---
+++ samplelibrary2/src/main/java/com/dwarsoftgames/samplelibrary2/ToastMessage.java
@@ -0,0 +1,14 @@
+package com.dwarsoftgames.samplelibrary2;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastMessage {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+    
+}
